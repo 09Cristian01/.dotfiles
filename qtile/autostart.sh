431f690f142fe352292a64f39a2cb9cecea96118
Cristian Guilarte <crislgf1@gmail.com>
2023-09-20 17:08:15 +0100

    Added dotfiles with soft links

---
+++ qtile/autostart.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#picom & #Compositor
+
+#unclutter --jitter 5 --idle 15 & #Hide cursor after 3sec
+
+feh --bg-scale /home/cristian/Images/Wallpapers/everforest-walls/nature/forest_stairs.jpg & #Set background
+
+#Add gui icons
+#udiskie -t &
+#nm-applet &
+#volumeicon &
+#cbatticon -u 5 &
